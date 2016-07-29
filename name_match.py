a20650ebe469abfd8c8f3903d2ca421f6a477891
raxxar1024 <yjw013809@gmail.com>
2016-07-29 09:09:00 +0800

    implement of find best match

--- name_match.py
+++ name_match.py
@@ -8,22 +8,67 @@ PASSWORD = "123456"
 HOST = "192.168.1.248"
 PORT = "5432"
 
+MATCH_LEVEL = 0.7
+
+
+def find_best_match(ori_str, str_set):
+    best_str = ""
+    best_factor = 0.0
+
+    for str in str_set:
+        factor = float(len(ori_str))/float(len(str[1]))
+        if factor > MATCH_LEVEL:
+            if factor > best_factor:
+                best_str = str[3]
+                best_factor = factor
+                if factor == 1:
+                    break
+
+    return best_factor, best_str
+
+
+def find_match(str):
+    conn_b = psycopg2.connect(database="place_names", user=USERNAME, password=PASSWORD, host=HOST, port=PORT)
+    cur_b = conn_b.cursor()
+    str = str.replace("'", " ")
+    sql_str = "SELECT * FROM %s WHERE place_name LIKE '%s';" % ("national_place_names", "%"+str+"%")
+    cur_b.execute(sql_str)
+    rows = cur_b.fetchall()
+
+    best_factor, best_str = find_best_match(str, rows)
+
+    conn_b.commit()
+    cur_b.close()
+    conn_b.close()
+
+    if best_factor > MATCH_LEVEL:
+        print best_factor, best_str
+    return best_factor, best_str
+
 
 def name_match(db_name, tb_name, idx):
-    conn = psycopg2.connect(database=db_name, user=USERNAME, password=PASSWORD, host=HOST, port=PORT)
-    cur = conn.cursor()
+    conn_a = psycopg2.connect(database=db_name, user=USERNAME, password=PASSWORD, host=HOST, port=PORT)
+    cur_a = conn_a.cursor()
 
     sql_str = "SELECT * FROM %s;" % tb_name
-    cur.execute(sql_str)
-    rows = cur.fetchall()
+    cur_a.execute(sql_str)
+    rows = cur_a.fetchall()
+    conn_a.commit()
 
     for i in rows:
         if i[idx] is not None and i[idx] != "":
-            print i[idx]
+            # print i[idx]
+            best_factor, best_str = find_match(i[idx])
+            if best_factor > MATCH_LEVEL:
+                best_str = i[idx] + "(" + best_str + ")"
+                update_str = "UPDATE %s SET name=%s WHERE osm_id=%s;" % (tb_name, "'"+best_str+"'", i[0])
+                print update_str
+                cur_a.execute(update_str)
+                conn_a.commit()
 
-    conn.commit()
-    cur.close()
-    conn.close()
+    # conn_a.commit()
+    cur_a.close()
+    conn_a.close()
 
 if __name__ == "__main__":
     # name_match("gis", "planet_osm_line", 38)
